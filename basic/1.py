61d4500dcf143eb99e06c733376ca77470bfc984
Vinicios Silva <106885386+vvinicios@users.noreply.github.com>
2026-02-22 16:30:26 +0000

    Primeiros arquivos

---
+++ basic/1.py
@@ -0,0 +1,19 @@
+import collections
+
+# Definir uma variável com uma collections (lista)
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+# Criando uma classe
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
